350586b1d20a39ed64039e732fad407df1bd2790
MatthewSong86 <54823079+MatthewSong86@users.noreply.github.com>
2022-07-14 10:37:35 -0700

    aa

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/Robot.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode.Robot;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.*;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.openftc.easyopencv.OpenCvCamera;
+
+public class Robot extends LinearOpMode {
+    public DcMotor fl,fr,bl,br;
+    public Servo sfl,sfr,sbl,sbr;
+    public OpenCvCamera camera;
+    public Robot(){
+        fl = hardwareMap.get(DcMotor.class, "fl");
+        fr = hardwareMap.get(DcMotor.class, "fl");
+        bl = hardwareMap.get(DcMotor.class, "fl");
+        br = hardwareMap.get(DcMotor.class, "fl");
+        sfl = hardwareMap.get(Servo.class, "sfl");
+        sfr = hardwareMap.get(Servo.class, "sfr");
+        sbl= hardwareMap.get(Servo.class, "sbl");
+        sbr = hardwareMap.get(Servo.class, "sbr");
+
+    }
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tele/simpleMec.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode.tele;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name="simpleMec")
+public class simpleMec {
+
+}
