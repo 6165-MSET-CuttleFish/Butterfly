967ff9a479fec9c0cda7ce46cff5735d0dc109e7
maartoon <85377718+maartoon@users.noreply.github.com>
2022-07-17 01:24:18 -0700

    party party yeAH

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/Robot.java
@@ -3,22 +3,32 @@ package org.firstinspires.ftc.teamcode.Robot;
 
 
 import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.*;
 import com.qualcomm.robotcore.hardware.Servo;
 
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 import org.openftc.easyopencv.OpenCvCamera;
 @Config
 public class Robot{
     public DcMotor fl,fr,bl,br;
-    public Servo sfl,sfr,sbl,sbr;
+    public Servo sfl, sfr, sbl, sbr;
     public OpenCvCamera camera;
     public boolean butterflyON;
     private final HardwareMap hardwareMap;
+    public BNO055IMU imu;
+    public Orientation lastAngles = new Orientation();
+    public double globalAngle;
 
     public Robot(OpMode opmode){
         hardwareMap = opmode.hardwareMap;
+
         fl = hardwareMap.get(DcMotor.class, "fl");
         fr = hardwareMap.get(DcMotor.class, "fr");
         bl = hardwareMap.get(DcMotor.class, "bl");
@@ -27,10 +37,19 @@ public class Robot{
         sfr = hardwareMap.get(Servo.class, "sfr");
         sbl = hardwareMap.get(Servo.class, "sbl");
         sbr = hardwareMap.get(Servo.class, "sbr");
+
+
         fl.setDirection(DcMotor.Direction.REVERSE);
         bl.setDirection(DcMotor.Direction.REVERSE);
         fr.setDirection(DcMotor.Direction.FORWARD);
         br.setDirection(DcMotor.Direction.FORWARD);
+
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.loggingEnabled = false;
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
 
         if (butterflyON) {
             butterflyOn();
@@ -43,16 +62,31 @@ public class Robot{
     }
     public void butterflyOn(){
         sfl.setPosition(0.47);
-        sbr.setPosition(0.45);
-        sfr.setPosition(0.5);
+        sbr.setPosition(0.80);
+        sfr.setPosition(0.50);
         sbl.setPosition(0.52);
 
     }
     public void butterflyOff(){
-        sfl.setPosition(0.44);
-        sbr.setPosition(0.75);
-        sfr.setPosition(0.70);
-        sbl.setPosition(0.80);
+        sfl.setPosition(0.45);
+        sbr.setPosition(0.68);
+        sfr.setPosition(0.67);
+        sbl.setPosition(0.8);
     }
+    public double getAngle() {
+        Orientation angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
 
+        double deltaAngle = angles.firstAngle - lastAngles.firstAngle;
+
+        if (deltaAngle < -180)
+            deltaAngle += 360;
+        else if (deltaAngle > 180)
+            deltaAngle -= 360;
+
+        globalAngle += deltaAngle;
+
+        lastAngles = angles;
+
+        return globalAngle;
+    }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tele/DriverCentricTele.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tele/DriverCentricTele.java
@@ -2,25 +2,34 @@ package org.firstinspires.ftc.teamcode.tele;
 
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
-import com.arcrobotics.ftclib.gamepad.*;
+import com.arcrobotics.ftclib.gamepad.ButtonReader;
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
 import com.arcrobotics.ftclib.gamepad.GamepadKeys;
+import com.arcrobotics.ftclib.gamepad.KeyReader;
+import com.arcrobotics.ftclib.gamepad.TriggerReader;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.Gamepad;
-import com.qualcomm.robotcore.hardware.*;
-import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.util.Range;
 
+import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.Position;
+import org.firstinspires.ftc.robotcore.external.navigation.Velocity;
 import org.firstinspires.ftc.teamcode.Robot.Robot;
+
 @Config
-@TeleOp(name = "simpleMec")
-public class simpleMec extends LinearOpMode {
-    public static double flPos; // = 0.45; //butterfly off, for tuning
-    public static double frPos; // = 0.65;
-    public static double blPos; // = 0.73;
-    public static double brPos; // = 0.67;
+@TeleOp(name = "DriverCentricTele")
+public class
+DriverCentricTele extends LinearOpMode {
+    public static double flPos; //= 0.44; butterfly off, for tuning
+    public static double frPos; // = 0.70;
+    public static double blPos; // = 0.80;
+    public static double brPos; // = 0.75;
     public double ninjaSlow = 0.7, multiplier;
-    Pose2d drivePower;
     Robot robot;
     GamepadEx primary;
     GamepadEx secondary;
@@ -30,6 +39,7 @@ public class simpleMec extends LinearOpMode {
     private double fb; //forward backward movement
     private double lr; //left right movement
     private double turn; //turning movement
+    double gamepadHeading, robotHeading, difference, fbControl, lrControl;
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -40,11 +50,14 @@ public class simpleMec extends LinearOpMode {
                 butterflyToggle = new ButtonReader(primary, GamepadKeys.Button.LEFT_BUMPER),
                 ninjaMode = new TriggerReader(primary, GamepadKeys.Trigger.LEFT_TRIGGER)
         };
-
         waitForStart();
+
         while (opModeIsActive()) {
             for (KeyReader reader : keyReaders)
                 reader.readValue();
+
+            telemetry.addData("1 imu heading", robot.lastAngles.firstAngle);
+            telemetry.addData("2 global heading", robot.globalAngle);
             drive();
 //            setServoPos();
             switchDrive();
@@ -58,32 +71,43 @@ public class simpleMec extends LinearOpMode {
 
     private void drive() {
         multiplier = 1;
+        final double startingHeading = robot.imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES).firstAngle;
+
         if (ninjaMode.isDown())
             multiplier = ninjaSlow;
         if (!robot.butterflyON) {
             //prevents accidental strafing
-            if (Math.abs(gamepad1.left_stick_x) > 0.2)
-                lr = gamepad1.left_stick_x;
-            else
-                lr = 0;
+            lr = gamepad1.left_stick_x;
             fb = -gamepad1.left_stick_y;
             turn = gamepad1.right_stick_x;
-            double r = Math.hypot(lr, fb);
+            double r = Range.clip(Math.hypot(lr, fb), 0, 1);
             double target = Math.atan2(fb, lr) - Math.PI / 4;
-            robot.fl.setPower((r * Math.cos(target) + turn) * multiplier);
-            robot.fr.setPower((r * Math.sin(target) - turn) * multiplier);
-            robot.bl.setPower((r * Math.sin(target) + turn) * multiplier);
-            robot.br.setPower((r * Math.cos(target) - turn) * multiplier);
+            gamepadHeading = Math.toDegrees(Math.atan2(fb, lr));
+            robotHeading = robot.getAngle();
+            difference = gamepadHeading - robotHeading;
+            telemetry.addData("gamepadHeading: ", gamepadHeading);
+            telemetry.addData("robotHeading: ", robotHeading);
+            telemetry.addData("startingHeading: ", startingHeading);
+            telemetry.addData("difference: ", difference);
+
+            lrControl = Math.cos(Math.toRadians(difference)) * r;
+            fbControl = Math.sin(Math.toRadians(difference)) * r;
+
+            robot.fl.setPower((fbControl * Math.abs(fbControl) + lrControl * Math.abs(lrControl) + (turn * 0.7)));
+            robot.fr.setPower((fbControl * Math.abs(fbControl) - lrControl * Math.abs(lrControl) - (turn * 0.7)));
+            robot.bl.setPower((fbControl * Math.abs(fbControl) - lrControl * Math.abs(lrControl) + (turn * 0.7)));
+            robot.br.setPower((fbControl * Math.abs(fbControl) + lrControl * Math.abs(lrControl) - (turn * 0.7)));
         } else {
             fb = -gamepad1.left_stick_y;
             turn = gamepad1.right_stick_x;
             robot.fl.setPower((fb + turn) * multiplier);
             robot.fr.setPower((fb - turn) * multiplier);
             robot.bl.setPower((fb + turn) * multiplier);
-            robot.br.setPower((fb - turn)* multiplier);
+            robot.br.setPower((fb - turn) * multiplier);
         }
 //        idle();
     }
+
     private void switchDrive() {
         if (butterflyToggle.wasJustPressed() && !robot.butterflyON) {
             robot.butterflyON = true;
@@ -95,10 +119,13 @@ public class simpleMec extends LinearOpMode {
             gamepad1.rumble(1.0, 1.0, 500);
         }
     }
+
     public void setServoPos() {
         robot.sfl.setPosition(flPos);
         robot.sfr.setPosition(frPos);
         robot.sbl.setPosition(blPos);
         robot.sbr.setPosition(brPos);
     }
+
+
 }
