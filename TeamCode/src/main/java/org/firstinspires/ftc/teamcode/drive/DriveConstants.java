1e91599779cad0bf7795ae39426545bdbae54a78
maartoon <85377718+maartoon@users.noreply.github.com>
2022-07-21 23:37:25 -0700

    almoooost

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -45,7 +45,7 @@ public class DriveConstants {
      */
     public static double WHEEL_RADIUS = 2; // in
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (motor) speed
-    public static double TRACK_WIDTH = 1; // in  64.86
+    public static double TRACK_WIDTH = 16.3125; //in
 
     /*
      * These are the feedforward parameters used to model the drive motor behavior. If you are using
